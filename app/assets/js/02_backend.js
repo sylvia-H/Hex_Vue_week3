c02cabe8c77dfa2f8c24994e7d183b9edd369115
sylvia-H <sylvie11gaia@gmail.com>
2022-02-14 17:08:16 +0800

    v-model 加上修飾符

--- app/assets/js/02_backend.js
+++ app/assets/js/02_backend.js
@@ -183,23 +183,3 @@ const app2 = {
 
 // 建立實體、掛載
 Vue.createApp(app2).mount('#app2');
-
-
-// {
-//     "title": "[賣]動物園造型衣服3",
-//     "category": "衣服2",
-//     "origin_price": 100,
-//     "price": 300,
-//     "unit": "個",
-//     "description": "Sit down please 名設計師設計",
-//     "content": "這是內容",
-//     "is_enabled": 1,
-//     "imageUrl": "主圖網址",
-//     "imagesUrl": [
-//       "圖片網址一",
-//       "圖片網址二",
-//       "圖片網址三",
-//       "圖片網址四",
-//       "圖片網址五"
-//     ]
-// }
